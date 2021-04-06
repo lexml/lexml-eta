b5edc39b243f9ac64363fdcebdf349dbb9be9ba2
jholanda <joao.holanda@gmail.com>
2021-04-06 16:22:09 -0300

    retirando @typescript-eslint/explicit-module-boundary-types da configuração do lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -52,5 +52,6 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'warn',
     '@typescript-eslint/no-non-null-assertion': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off'
   },
 };
