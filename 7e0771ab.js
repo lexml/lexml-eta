f8291a49265cef8a783a617ad9b7ff428f869f92
jholanda <joao.holanda@gmail.com>
2021-04-28 18:47:01 +0000

    Update gh-pages to output generated at 1972892

---
+++ 7e0771ab.js
@@ -0,0 +1,15 @@
+import{R as t,r as e,f as s,t as r,o as a}from"./e94213f9.js";class d extends t{get disabled(){return!0}[e](t){super[e](t),this[s]&&this.setAttribute("aria-hidden","true")}}let i,o=t=>t;class n extends d{get[r](){return a.html(i||(i=o`
+      <style>
+        :host {
+          padding: 0 !important;
+        }
+
+        hr {
+          border-bottom-width: 0px;
+          border-color: #fff; /* Ends up as light gray */
+          border-top-width: 1px;
+          margin: 0.25em 0;
+        }
+      </style>
+      <hr>
+    `))}}customElements.define("elix-menu-separator",class extends n{});
