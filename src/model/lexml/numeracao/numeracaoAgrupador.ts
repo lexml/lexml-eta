25e5a19be53da7cf6097143a4c2a1a019774802d
jholanda <joao.holanda@gmail.com>
2021-11-16 20:31:09 -0300

    atualizados os testes para agrupadores únicos

--- src/model/lexml/numeracao/numeracaoAgrupador.ts
+++ src/model/lexml/numeracao/numeracaoAgrupador.ts
@@ -25,7 +25,7 @@ export function NumeracaoAgrupador<TBase extends Constructor>(Base: TBase): any 
       const prefixo = dispositivo.descricao === undefined ? dispositivo.name ?? '' : dispositivo.descricao.toLocaleUpperCase();
 
       if (this.numero === undefined) {
-        this.rotulo = '\u201C' + dispositivo.tipo;
+        this.rotulo = dispositivo.tipo;
       } else if (this.numero !== undefined && !isNumeracaoValida(this.numero)) {
         this.rotulo = prefixo + this.numero;
       } else if (dispositivo.isDispositivoAlteracao && isDispositivoCabecaAlteracao(dispositivo)) {
