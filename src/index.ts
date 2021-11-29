7be856d876fe32e1e0110eed6e18687ad32e5ba2
Robson Barros <robsonbarrosdf@gmail.com>
2021-11-29 12:35:51 -0300

    Ajuste nas referências para os componentes do Elix

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,3 @@ export { EditorComponent } from './components/editor/editor.component';
 export { ElementoComponent } from './components/elemento/elemento.component';
 export { LexmlEtaComponent } from './components/lexml-eta.component';
 export { HelpComponent } from './components/help.component';
-
-import '/node_modules/elix/define/Toast.js';
-import '/node_modules/elix/define/Dialog.js';
-import '/node_modules/elix/define/AlertDialog.js';
-import '/node_modules/elix/define/MenuSeparator.js';
-import '/node_modules/elix/define/MenuItem.js';
-import '/node_modules/elix/define/MenuButton.js';
