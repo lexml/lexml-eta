99dcfb260b6bf5a05b6f1c0e7761c37779220992
jholanda <joao.holanda@gmail.com>
2022-01-18 19:52:37 -0300

    ajuste da validação de artigo

--- src/model/lexml/conteudo/conteudoValidator.ts
+++ src/model/lexml/conteudo/conteudoValidator.ts
@@ -153,7 +153,6 @@ export const validaTextoDispositivo = (dispositivo: Dispositivo): Mensagem[] => 
     !hasFilhos(dispositivo) &&
     !dispositivo.hasAlteracao() &&
     !isUnicoMesmoTipo(dispositivo) &&
-    !isUltimoMesmoTipo(dispositivo) &&
     !hasIndicativoContinuacaoSequencia(dispositivo)
   ) {
     mensagens.push({
