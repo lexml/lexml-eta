7e1d26ad50902c91dfce04c3d12258a350805c5c
jholanda <joao.holanda@gmail.com>
2022-02-07 20:08:15 -0300

    acertando a alteração na geração do json

--- src/model/lexml/documento/conversor/buildJsonixFromProjetoNorma.ts
+++ src/model/lexml/documento/conversor/buildJsonixFromProjetoNorma.ts
@@ -1,5 +1,6 @@
 import { Artigo, Dispositivo } from '../../../dispositivo/dispositivo';
 import { isAgrupador, isArtigo, isCaput } from '../../../dispositivo/tipo';
+import { buildHref, buildIdAlteracao } from '../../util/idUtil';
 import { isNorma, ProjetoNorma } from '../projetoNorma';
 
 export const buildJsonixFromProjetoNorma = (projetoNorma: ProjetoNorma, urn: string): any => {
@@ -84,20 +85,6 @@ const buildTree = (dispositivo: Dispositivo, obj: any): any => {
     tree = obj.lXcontainersOmissis = [];
   }
 
-  if (isArtigo(dispositivo)) {
-    const node = buildNode((dispositivo as Artigo).caput!);
-    tree.push(node);
-  }
-
-  if (dispositivo.hasAlteracao()) {
-    dispositivo.alteracoes!.filhos?.forEach(filho => {
-      const node = buildNode(filho);
-      tree.push(node);
-
-      buildTree(filho, node.value);
-    });
-  }
-
   dispositivo.filhos?.forEach(filho => {
     const node = buildNode(filho);
     tree.push(node);
@@ -105,7 +92,35 @@ const buildTree = (dispositivo: Dispositivo, obj: any): any => {
     buildTree(filho, node.value);
   });
 
+  if (isArtigo(dispositivo)) {
+    const node = buildNode((dispositivo as Artigo).caput!);
+    buildAlteracaoSeNecessario(dispositivo, node.value);
+
+    tree.push(node);
+  }
+
   return tree;
+};
+
+const buildAlteracaoSeNecessario = (dispositivo: Dispositivo, node: any): void => {
+  if (dispositivo.hasAlteracao()) {
+    node['alteracao'] = {
+      TYPE_NAME: 'br_gov_lexml__1.Alteracao',
+      base: '',
+      id: '',
+      content: [],
+    };
+
+    node.alteracao.base = dispositivo.alteracoes?.base ?? '';
+    node.alteracao.id = buildIdAlteracao((dispositivo as Artigo).caput!);
+
+    dispositivo.alteracoes!.filhos?.forEach(filho => {
+      const n = buildNode(filho);
+      node.alteracao.content.push(n);
+
+      buildTree(filho, n.value);
+    });
+  }
 };
 
 const buildNode = (dispositivo: Dispositivo): any => {
@@ -128,9 +143,19 @@ const buildNode = (dispositivo: Dispositivo): any => {
 };
 
 const buildDispositivo = (dispositivo: Dispositivo, value: any): void => {
-  value.id = dispositivo.id;
-  if (!isCaput(dispositivo)) {
+  value['href'] = buildHref(dispositivo);
+
+  value['id'] = dispositivo.id;
+
+  if (dispositivo.rotulo && /^["”“].*/.test(dispositivo.rotulo)) {
+    value['abreAspas'] = 's';
+    value.rotulo = dispositivo.rotulo.substring(1);
+  } else if (!isCaput(dispositivo)) {
     value.rotulo = dispositivo.rotulo;
+  }
+
+  if (isCaput(dispositivo) && /”.*(NR)/.test(dispositivo.texto)) {
+    value['fechaAspas'] = 's';
   }
 
   if (isAgrupador(dispositivo)) {
@@ -139,7 +164,7 @@ const buildDispositivo = (dispositivo: Dispositivo, value: any): void => {
       content: buildContent(dispositivo),
     };
   } else if (!isArtigo(dispositivo)) {
-    value.p = [{ TYPE_NAME: 'br_gov_lexml__1.GenInline', content: buildContent(dispositivo) }];
+    value['p'] = [{ TYPE_NAME: 'br_gov_lexml__1.GenInline', content: buildContent(dispositivo) }];
   }
 };
 
