8b74ff02492dc4b220ee0f1623b97eddcf936e56
jholanda <joao.holanda@gmail.com>
2022-03-10 20:21:53 -0300

    Refatorando comando de emenda para torná-lo independente

--- src/model/lexml/acao/recuperarArticulacaoAtualizadaAction.ts
+++
@@ -1,7 +0,0 @@
-export const ARTICULACAO_ATUALIZADA = 'ARTICULACAO_ATUALIZADA';
-
-export const recuperarArticulacaoAtualizadaAction = (): any => {
-  return {
-    type: ARTICULACAO_ATUALIZADA,
-  };
-};
