b1fc8bc1c4fff546c34527fb32f8ee7ceba73e97
Robson Barros <robson.barros@camara.leg.br>
2022-03-31 21:45:46 +0100

    implementando componente tipo-autoria

--- src/components/autoria/tipo-autoria.component.ts
+++ src/components/autoria/tipo-autoria.component.ts
@@ -1,22 +1,34 @@
 import { html, TemplateResult } from 'lit-html';
-import { LitElement, customElement, PropertyValues } from 'lit-element';
+import { LitElement, customElement, PropertyValues, property } from 'lit-element';
+import { Autoria } from './../../model/autoria/autoria';
 
 @customElement('lexml-tipo-autoria')
 export class TipoAutoria extends LitElement {
+  @property({ type: Object })
+  autoria?: Autoria;
+
+  createRenderRoot(): LitElement {
+    return this;
+  }
+
   update(changedProperties: PropertyValues): void {
     super.update(changedProperties);
   }
 
+  atualizarObjeto(e: Event): void {
+    this.autoria!.tipo = (e.target as HTMLInputElement).value;
+  }
+
   render(): TemplateResult {
     return html`
-      <div>
+      <div @input=${this.atualizarObjeto}>
         <div>
           <p><strong>Tipo de autoria</strong></p>
 
-          <input type="radio" id="opt-parlamentar" />
+          <input type="radio" id="opt-parlamentar" name="tipoAutoria" value="Parlamentar" ?checked=${this.autoria?.tipo === 'Parlamentar'} />
           <label for="opt-parlamentar">Parlamentar</label>
 
-          <input type="radio" id="opt-comissao" />
+          <input type="radio" id="opt-comissao" name="tipoAutoria" value="Comissão" ?checked=${this.autoria?.tipo === 'Comissão'} />
           <label for="opt-comissao">Comissão</label>
         </div>
       </div>
