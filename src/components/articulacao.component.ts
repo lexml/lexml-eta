31c72604f65ee9646c6dd47bac09d18617be3ef7
Erivânio Vasconcelos <erivanio@MacBook-Air-de-Erivanio.local>
2022-04-25 12:13:30 -0300

    adiciona abas ao componente articulação, com texto, justificativa, data e autoria

--- src/components/articulacao.component.ts
+++ src/components/articulacao.component.ts
@@ -1,17 +1,41 @@
 import { LitElement, html, TemplateResult } from 'lit';
-import { customElement, property } from 'lit/decorators.js';
+import { customElement, property, state } from 'lit/decorators.js';
 
 import { connect } from 'pwa-helpers';
 
 import { Elemento } from '../model/elemento';
 import { rootStore } from '../redux/store';
 
+// import '@shoelace-style/shoelace/dist/themes/light.css';
+import '@shoelace-style/shoelace';
+
+import { Autoria, Parlamentar } from '../../src/model/emenda/emenda';
+
 @customElement('lexml-eta-articulacao')
 export class ArticulacaoComponent extends connect(rootStore)(LitElement) {
   @property({ type: Array }) elementos: Elemento[] = [];
+  @property({ type: String }) textoJustificativa = "<p class='align-center'>texto centralizado</p>";
+  @state()
+  autoria = new Autoria();
+
+  @state()
+  parlamentares: Parlamentar[] = [];
+
+  async getParlamentares(): Promise<Parlamentar[]> {
+    const _parlamentares = await (await fetch('https://emendas-api.herokuapp.com/parlamentares')).json();
+    return _parlamentares.map(p => ({
+      identificacao: p.id,
+      nome: p.nome,
+      sexo: p.sexo,
+      siglaPartido: p.siglaPartido,
+      siglaUF: p.siglaUF,
+      siglaCasaLegislativa: p.siglaCasa,
+    }));
+  }
 
   constructor() {
     super();
+    this.getParlamentares().then(parlamentares => (this.parlamentares = parlamentares));
     this.tabIndex = -1;
   }
 
@@ -21,6 +45,7 @@ export class ArticulacaoComponent extends connect(rootStore)(LitElement) {
 
   render(): TemplateResult {
     return html`
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.0.0-beta.73/dist/themes/light.css" />
       <style>
         lexml-eta-editor {
           display: block;
@@ -34,7 +59,22 @@ export class ArticulacaoComponent extends connect(rootStore)(LitElement) {
           box-shadow: none;
         }
       </style>
-      <lexml-eta-editor></lexml-eta-editor>
+      <sl-tab-group>
+        <sl-tab slot="nav" panel="lexml-eta">Texto</sl-tab>
+        <sl-tab slot="nav" panel="justificativa">Justificativa</sl-tab>
+        <sl-tab slot="nav" panel="autoria">Data e Autoria</sl-tab>
+        <sl-tab-panel name="lexml-eta">
+          <lexml-eta-editor></lexml-eta-editor>
+        </sl-tab-panel>
+        <sl-tab-panel name="justificativa">
+          <lexml-emenda-justificativa texto=${this.textoJustificativa}></lexml-emenda-justificativa>
+        </sl-tab-panel>
+        <sl-tab-panel name="autoria">
+          <lexml-data></lexml-data>
+          <hr />
+          <lexml-autoria .parlamentares=${this.parlamentares} .autoria=${this.autoria}></lexml-autoria>
+        </sl-tab-panel>
+      </sl-tab-group>
     `;
   }
 }
