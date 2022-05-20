eb6b791ea9b673015c2f6aaadf6d6f113918e3a7
Erivânio Vasconcelos <erivanio@MacBook-Air-de-Erivanio.local>
2022-05-20 12:00:33 -0300

    atualiza os alertas quando o state do store aletaReducer for alterado

--- src/components/alertas/alertas.component.ts
+++ src/components/alertas/alertas.component.ts
@@ -1,10 +1,14 @@
 import { LitElement, html, TemplateResult, css } from 'lit';
-import { customElement } from 'lit/decorators.js';
+import { customElement, property } from 'lit/decorators.js';
+import { map } from 'lit/directives/map.js';
 import '@shoelace-style/shoelace/dist/components/alert/alert.js';
 import '@shoelace-style/shoelace/dist/components/icon/icon.js';
+import { connect } from 'pwa-helpers';
+import { rootStore } from '../../redux/store';
+import { Alerta } from '../../model/alerta/alerta';
 
 @customElement('lexml-eta-alertas')
-export class AlertasComponent extends LitElement {
+export class AlertasComponent extends connect(rootStore)(LitElement) {
   static styles = css`
     sl-alert {
       --box-shadow: var(--sl-shadow-x-large);
@@ -12,28 +16,30 @@ export class AlertasComponent extends LitElement {
     }
   `;
 
+  @property({ type: Array }) alertas: Alerta[] = [];
+
+  stateChanged(state: any): void {
+    this.alertas = state.alertaReducer.alertas;
+  }
+
+  getAlertIcon(tipo: string): TemplateResult {
+    if (tipo === 'success') {
+      return html`<sl-icon slot="icon" name="check2-circle"></sl-icon>`;
+    } else if (tipo === 'warning') {
+      return html`<sl-icon slot="icon" name="exclamation-triangle"></sl-icon>`;
+    } else if (tipo === 'danger') {
+      return html`<sl-icon slot="icon" name="exclamation-octagon"></sl-icon>`;
+    } else {
+      return html`<sl-icon slot="icon" name="info-circle"></sl-icon>`;
+    }
+  }
+
   render(): TemplateResult {
     return html`
-      <sl-alert variant="success" open closable class="alert-closable">
-        <sl-icon slot="icon" name="check2-circle"></sl-icon>
-        Cada emenda somente pode referir-se a apenas um dispositivo, salvo se houver correlação entre dispositivos. Verifique se há correlação entre os dispositivos emendados antes
-        antes de submetê-la.
-      </sl-alert>
-      <sl-alert variant="warning" open closable class="alert-closable">
-        <sl-icon slot="icon" name="exclamation-triangle"></sl-icon>
-        Cada emenda somente pode referir-se a apenas um dispositivo, salvo se houver correlação entre dispositivos. Verifique se há correlação entre os dispositivos emendados antes
-        antes de submetê-la.
-      </sl-alert>
-      <sl-alert variant="danger" open closable class="alert-closable">
-        <sl-icon slot="icon" name="exclamation-octagon"></sl-icon>
-        Cada emenda somente pode referir-se a apenas um dispositivo, salvo se houver correlação entre dispositivos. Verifique se há correlação entre os dispositivos emendados antes
-        antes de submetê-la.
-      </sl-alert>
-      <sl-alert variant="primary" open closable class="alert-closable">
-        <sl-icon slot="icon" name="info-circle"></sl-icon>
-        Cada emenda somente pode referir-se a apenas um dispositivo, salvo se houver correlação entre dispositivos. Verifique se há correlação entre os dispositivos emendados antes
-        antes de submetê-la.
-      </sl-alert>
+      ${map(
+        this.alertas,
+        alerta => html` <sl-alert variant="${alerta.tipo}" open closable class="alert-closable"> ${this.getAlertIcon(alerta.tipo)} ${alerta.mensagem} </sl-alert> `
+      )}
     `;
   }
 }
