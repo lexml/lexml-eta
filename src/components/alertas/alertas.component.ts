aaf52c0523be7d676e6b3d311ff898f17f2cbc53
Erivânio Vasconcelos <erivanio@MacBook-Air-de-Erivanio.local>
2022-05-23 13:56:33 -0300

    adiciona funcao de limpar alertas

--- src/components/alertas/alertas.component.ts
+++ src/components/alertas/alertas.component.ts
@@ -6,6 +6,7 @@ import '@shoelace-style/shoelace/dist/components/icon/icon.js';
 import { connect } from 'pwa-helpers';
 import { rootStore } from '../../redux/store';
 import { Alerta } from '../../model/alerta/alerta';
+import { limparAlertas } from '../../redux/alerta/reducer/actions';
 
 @customElement('lexml-eta-alertas')
 export class AlertasComponent extends connect(rootStore)(LitElement) {
@@ -34,11 +35,17 @@ export class AlertasComponent extends connect(rootStore)(LitElement) {
     }
   }
 
+  limparAlertas(): void {
+    rootStore.dispatch(limparAlertas());
+  }
+
   render(): TemplateResult {
     return html`
+      <!-- <button @click=${this.limparAlertas}>Apagar alertas</button> -->
       ${map(
         this.alertas,
-        alerta => html` <sl-alert variant="${alerta.tipo}" open closable class="alert-closable"> ${this.getAlertIcon(alerta.tipo)} ${alerta.mensagem} </sl-alert> `
+        alerta =>
+          html` <sl-alert variant="${alerta.tipo}" id="${alerta.id}" open closable class="alert-closable"> ${this.getAlertIcon(alerta.tipo)} ${alerta.mensagem} </sl-alert> `
       )}
     `;
   }
