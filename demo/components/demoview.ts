65b13f1c4816f68ca885b2a539bb560bd14174ca
Erivânio Vasconcelos <erivanio.vanin@gmail.com>
2022-05-26 10:27:31 -0300

    feat: redireciona para a aba de texto ao carregar um novo projeto
    closes #160 
    
    * muda para aba de texto ao carregar um projeto
    
    * corrige a função selecionaArquivo para mostrar o comando emenda ao abrir arquivo no demoview

--- demo/components/demoview.ts
+++ demo/components/demoview.ts
@@ -139,9 +139,9 @@ export class DemoView extends LitElement {
           this.arquivoProjetoNorma = result.projetoNorma;
           this.projetoNorma = result.projetoNorma?.value?.projetoNorma?.norma?.parteInicial?.epigrafe?.content[0] ?? '';
           this.getElement('lexml-emenda-comando').emenda = result.emenda.comandoEmenda;
-          this.getElement('#comandoEmenda')!['style'].display = 'block';
+          this.getElement('lexml-emenda-comando')!['style'].display = 'block';
           this.getElement('.wrapper').style['grid-template-columns'] = '2fr 1fr';
-          document.querySelector('lexml-emenda')!['style'].display = 'block';
+          this.getElement('lexml-emenda')!['style'].display = 'block';
         }
       };
     }
