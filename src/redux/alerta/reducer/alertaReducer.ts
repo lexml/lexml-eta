7cfc9dbbefa700378d634778d5c9101a91d8157f
Erivânio Vasconcelos <erivanio@MacBook-Air-de-Erivanio.local>
2022-05-30 10:27:50 -0300

    altera adicionar_alerta para não adicionar alertas com mesmo id

--- src/redux/alerta/reducer/alertaReducer.ts
+++ src/redux/alerta/reducer/alertaReducer.ts
@@ -1,9 +1,15 @@
 export const alertaReducer = (state = { alertas: [] }, action: any): any => {
   switch (action.type) {
     case 'ADICIONAR_ALERTA':
-      return {
-        alertas: [...state.alertas, action.alerta],
-      };
+      if (state.alertas.filter(({ id }) => id === action.alerta.id).length > 0) {
+        return {
+          alertas: [...state.alertas],
+        };
+      } else {
+        return {
+          alertas: [...state.alertas, action.alerta],
+        };
+      }
     case 'REMOVER_ALERTA':
       return {
         alertas: state.alertas?.filter(({ id }) => id !== action.id),
