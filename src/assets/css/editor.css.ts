dc411ead112b45b6b73e65c5e5e3f8063cd04e34
arthurcordeiro <arthur.cordeiro@gmail.com>
2022-06-14 13:38:20 -0300

    feat: cria CSS para destacar dispositivo existente em norma alterada e troca borda por box-shadow no rótulo #226

--- src/assets/css/editor.css.ts
+++ src/assets/css/editor.css.ts
@@ -212,7 +212,25 @@ export const editorStyles = html`
 
     .rotulo {
       cursor: pointer;
-      border-bottom: 1px dashed;
+      /* border-bottom: 1px dashed; */
+      -webkit-box-shadow: 0px -1px 0px green inset;
+      -moz-box-shadow: 0px -1px 0px green inset;
+      box-shadow: 0px -1px 0px green inset;
+    }
+
+    [existenanormaalterada='true'] label:before {
+      position: relative;
+      content: 'Existente';
+      left: 0;
+      top: -1px;
+      margin-right: 5px;
+      border: 1px solid green;
+      padding: 0 4px;
+      font-weight: normal;
+      -webkit-border-radius: 4px;
+      -moz-border-radius: 4px;
+      border-radius: 4px;
+      font-size: 12px;
     }
 
     .texto__rotulo {
