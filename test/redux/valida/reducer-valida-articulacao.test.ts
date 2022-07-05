b3b85c5649c015045f465ff65232a400947bde00
Erivânio Vasconcelos <erivanio@MacBook-Air-de-Erivanio.local>
2022-07-05 10:57:19 -0300

    inicializa state com ui vazio

--- test/redux/valida/reducer-valida-articulacao.test.ts
+++ test/redux/valida/reducer-valida-articulacao.test.ts
@@ -13,7 +13,7 @@ describe('Testando a atualização de texto artigos e dispositivos de artigo', (
     articulacao.renumeraArtigos();
     state = {
       articulacao,
-      ui: { events: [] },
+      ui: {},
     };
   });
   describe('Inicialização do teste ', () => {
