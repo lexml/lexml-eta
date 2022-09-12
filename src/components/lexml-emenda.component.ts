2dea70211340c7d1b6405f08311858bee17fdbfc
Marcos Fragomeni <marcos.fragomeni@gmail.com>
2022-09-12 12:23:52 -0300

    refactor: aponta svc parlamentares p/ edito-emendas

--- src/components/lexml-emenda.component.ts
+++ src/components/lexml-emenda.component.ts
@@ -70,9 +70,8 @@ export class LexmlEmendaComponent extends connect(rootStore)(LitElement) {
   private slSplitPanel!: any;
 
   async getParlamentares(): Promise<Parlamentar[]> {
-    const _parlamentares = await (await fetch('https://emendas-api.herokuapp.com/parlamentares')).json();
-    // const _response = await fetch('api/parlamentares');
-    // const _parlamentares = await _response.json();
+    const _response = await fetch('api/parlamentares');
+    const _parlamentares = await _response.json();
     return _parlamentares.map(p => ({
       identificacao: p.id,
       nome: p.nome,
