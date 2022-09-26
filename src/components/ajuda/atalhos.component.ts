34d0d7ab00e46dc91c93efed25442301893635be
jholanda <joao.holanda@gmail.com>
2022-09-26 17:26:10 -0300

    refactor: padronizar sinal + em teclas de atalhos
    Closes #447

--- src/components/ajuda/atalhos.component.ts
+++ src/components/ajuda/atalhos.component.ts
@@ -1,4 +1,4 @@
-import { LitElement, html, TemplateResult, css } from 'lit';
+import { css, html, LitElement, TemplateResult } from 'lit';
 import { customElement } from 'lit/decorators.js';
 
 @customElement('lexml-eta-atalhos')
@@ -29,29 +29,29 @@ export class AtalhosComponent extends LitElement {
     return html`
       <div class="lx-eta-help">
         <div class="lx-eta-help-content">
-          <div><b>ctrl-shift-a</b>&nbsp;-&nbsp;Seleciona o texto do dispositivo atual</div>
-          <div><b>ctrl-a</b>&nbsp;-&nbsp;Seleciona todos os dispositivos da articulação</div>
-          <div><b>ctrl-home</b>&nbsp;-&nbsp;Vai para o primeiro dispositivo</div>
-          <div><b>ctrl-end</b>&nbsp;-&nbsp;Vai para o último dispositivo</div>
-          <div><b>alt-&uarr;</b>&nbsp;-&nbsp;Move o dispositivo acima</div>
-          <div><b>alt-&darr;</b>&nbsp;-&nbsp;Move o dispositivo abaixo</div>
-          <div><b>ctrl-f;</b>&nbsp;-&nbsp;Localizar texto</div>
+          <div><b>Ctrl+Shift-A</b>&nbsp;-&nbsp;Seleciona o texto do dispositivo atual</div>
+          <div><b>Ctrl+A</b>&nbsp;-&nbsp;Seleciona todos os dispositivos da articulação</div>
+          <div><b>Ctrl+Home</b>&nbsp;-&nbsp;Vai para o primeiro dispositivo</div>
+          <div><b>Ctrl+End</b>&nbsp;-&nbsp;Vai para o último dispositivo</div>
+          <div><b>Alt+&uarr;</b>&nbsp;-&nbsp;Move o dispositivo acima</div>
+          <div><b>Alt+&darr;</b>&nbsp;-&nbsp;Move o dispositivo abaixo</div>
+          <div><b>Ctrl+F;</b>&nbsp;-&nbsp;Localizar texto</div>
           <hr></hr>
-          <div><b>enter</b>&nbsp;-&nbsp;Finaliza a edição do dispositivo atual e cria um novo</div>
-          <div><b>ctrl-d</b>&nbsp;-&nbsp;Remove dispositivo atual</div>
-          <div><b>ctrl-z</b>&nbsp;-&nbsp;Desfaz ultima alteração</div>
-          <div><b>ctrl-y</b>&nbsp;-&nbsp;Refaz alteração</div>
+          <div><b>Enter</b>&nbsp;-&nbsp;Finaliza a edição do dispositivo atual e cria um novo</div>
+          <div><b>Ctrl+D</b>&nbsp;-&nbsp;Remove dispositivo atual</div>
+          <div><b>Ctrl+Z</b>&nbsp;-&nbsp;Desfaz ultima alteração</div>
+          <div><b>Ctrl+Y</b>&nbsp;-&nbsp;Refaz alteração</div>
           <hr></hr>
-          <div><b>ctrl-alt-A</b>&nbsp;-&nbsp;Transforma o dispositivo em artigo</div>
-          <div><b>ctrl-alt-L</b>&nbsp;-&nbsp;Transforma o dispositivo em alínea</div>
-          <div><b>ctrl-alt-N</b>&nbsp;-&nbsp;Transforma o dispositivo em inciso</div>
-          <div><b>ctrl-alt-O</b>&nbsp;-&nbsp;Transforma o dispositivo em omissis</div>
-          <div><b>ctrl-alt-P</b>&nbsp;-&nbsp;Transforma o dispositivo em parágrafo</div>
-          <div><b>ctrl-alt-T</b>&nbsp;-&nbsp;Transforma o dispositivo em item</div>
-          <div><b>tab</b>&nbsp;-&nbsp;Indenta para a direita o dispositivo, transformando-o no tipo mais provável</div>
-          <div><b>shift-tab</b>&nbsp;-&nbsp;Recua o dispositivo para a esquerda, transformando-o no tipo mais provável</div>
+          <div><b>Ctrl+Alt+A</b>&nbsp;-&nbsp;Transforma o dispositivo em artigo</div>
+          <div><b>Ctrl+Alt+L</b>&nbsp;-&nbsp;Transforma o dispositivo em alínea</div>
+          <div><b>Ctrl+Alt+N</b>&nbsp;-&nbsp;Transforma o dispositivo em inciso</div>
+          <div><b>Ctrl+Alt+O</b>&nbsp;-&nbsp;Transforma o dispositivo em omissis</div>
+          <div><b>Ctrl+Alt+P</b>&nbsp;-&nbsp;Transforma o dispositivo em parágrafo</div>
+          <div><b>Ctrl+Alt+T</b>&nbsp;-&nbsp;Transforma o dispositivo em item</div>
+          <div><b>Tab</b>&nbsp;-&nbsp;Indenta para a direita o dispositivo, transformando-o no tipo mais provável</div>
+          <div><b>Shift+Tab</b>&nbsp;-&nbsp;Recua o dispositivo para a esquerda, transformando-o no tipo mais provável</div>
           <hr></hr>
-          <div><b>ctrl-alt-R</b>&nbsp;-&nbsp;Renumera o dispositivo</div>
+          <div><b>Ctrl+Alt+R</b>&nbsp;-&nbsp;Renumera o dispositivo</div>
         </div>
       </div>
     `;
