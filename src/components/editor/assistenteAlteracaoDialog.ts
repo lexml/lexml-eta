544dc741420a01816ae51bf73feaa89ed538872f
Marcos Fragomeni <marcos.fragomeni@gmail.com>
2022-09-28 12:56:44 -0300

    feat: revisao textos assistente alt norma
    refs #334

--- src/components/editor/assistenteAlteracaoDialog.ts
+++ src/components/editor/assistenteAlteracaoDialog.ts
@@ -6,7 +6,7 @@ import { validaDispositivoAssistente } from '../../model/lexml/numeracao/parserR
 export async function assistenteAlteracaoDialog(elemento: Elemento, quill: any, store: any, action: any): Promise<any> {
   const dialogElem = document.createElement('sl-dialog');
   document.body.appendChild(dialogElem);
-  dialogElem.label = 'Assistente de Alteração de Norma';
+  dialogElem.label = 'Assistente de alteração de norma';
   dialogElem.addEventListener('sl-request-close', (event: any) => {
     if (event.detail.source === 'overlay') {
       event.preventDefault();
@@ -14,7 +14,12 @@ export async function assistenteAlteracaoDialog(elemento: Elemento, quill: any, 
   });
 
   const content = document.createRange().createContextualFragment(`
-  <style></style>
+  <style>
+    .ajuda {
+      font-size: var(--sl-font-size-small);
+      font-weight: normal;
+    }
+  </style>
   <div class="input-validation-required">
     <sl-select name="tipoNorma" id="tipoNorma" label="Tipo da norma" clearable>
       <sl-menu-item value="decreto">Decreto</sl-menu-item>
@@ -27,18 +32,27 @@ export async function assistenteAlteracaoDialog(elemento: Elemento, quill: any, 
     <br/>
     <sl-input name="numeroNorma" id="numeroNorma" placeholder="8666 (número sem ponto)" label="Número" clearable></sl-input>
     <br/>
-    
+
     <sl-radio-group fieldset>
       <sl-radio name="informarData" id="informarDataCompleta" value="informarDataCompleta" checked="true">Data</sl-radio>
       <sl-input type="date" name="dataNorma" id="dataNorma" clearable/></sl-input>
-      
+
       <sl-radio name="informarData" id="informarApenasAno" value="informarApenasAno">Ano</sl-radio>
       <sl-input name="anoNorma" id="anoNorma"></sl-input>
 
     </sl-radio-group>
     <p>
-    <sl-input name="dispositivos" id="dispositivos" placeholder="ex: inciso I do § 3º do Art.1º" help-text="Pode ser utilizado 'parágrafo', 'par' ou § para referenciar parágrafo" label="Dispositivo da norma" clearable></sl-input>
+    <sl-input name="dispositivos" id="dispositivos" placeholder="ex: inciso I do § 3º do Art.1º" label="Dispositivo da norma" clearable></sl-input>
     </p>
+    <p class="ajuda" style="margin-block-end: 0;">
+      Terminar sempre com o artigo, por exemplo:
+    </p>
+    <ul class="ajuda" style="margin-block-start: .3em; margin-block-end: 0;">
+      <li>item 9 da alínea b do inciso V do parágrafo 4º do artigo 12-B</li>
+      <li>ali c, inc X, par 6, art 1</li>
+      <li>§ 8 art 32</li>
+      <li>parágrafo único do art 7º</li>
+    </ul>
   </div>
   <br/>
   <sl-alert variant="warning" closable class="alert-closable">
