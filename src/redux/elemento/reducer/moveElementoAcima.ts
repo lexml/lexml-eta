87c5e89f014c4ec5fbc5ebc095dd440882680d4a
Robson Barros <robson.barros@camara.leg.br>
2023-02-15 23:34:57 +0000

    refactor: removendo console.log em "moveElementoAcima.ts"

--- src/redux/elemento/reducer/moveElementoAcima.ts
+++ src/redux/elemento/reducer/moveElementoAcima.ts
@@ -91,7 +91,7 @@ export const moveElementoAcima = (state: any, action: any): State => {
   // Segundo elemento será usado para marcar o elemento em caso de "undo"
   eventos.add(StateType.ElementoMarcado, [createElemento(atual), action.atual]);
   eventos.add(StateType.ElementoSelecionado, [createElemento(atual)]);
-  console.log(eventos);
+  // console.log(eventos);
   return {
     articulacao: state.articulacao,
     modo: state.modo,
