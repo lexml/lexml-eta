27b53010055d2e19ffd991ea8b0c62984e61da4b
Robson Barros <robsonbarrosdf@gmail.com>
2023-02-27 10:01:51 -0300

    feat/fix: bloqueando exclusão de agrupador caso resultado da ação implique em inconsistência na articulação; corrigindo reposicionamento de artigos após exclusão de agrupador

--- src/redux/elemento/reducer/removeElemento.ts
+++ src/redux/elemento/reducer/removeElemento.ts
@@ -1,7 +1,13 @@
 import { createElemento } from './../../../model/elemento/elementoUtil';
 import { StateType } from './../../state';
-import { getDispositivoPosterior, getDispositivoAnterior } from './../../../model/lexml/hierarquia/hierarquiaUtil';
-import { isAgrupador } from '../../../model/dispositivo/tipo';
+import {
+  getDispositivoPosterior,
+  getDispositivoAnterior,
+  getPrimeiroAgrupadorNaArticulacao,
+  hasEmenta,
+  getTiposAgrupadorArtigoPermitidosNaArticulacao,
+} from './../../../model/lexml/hierarquia/hierarquiaUtil';
+import { isAgrupador, isArticulacao, isArtigo } from '../../../model/dispositivo/tipo';
 import { getDispositivoFromElemento } from '../../../model/elemento/elementoUtil';
 import { isAcaoPermitida } from '../../../model/lexml/acao/acaoUtil';
 import { RemoverElemento } from '../../../model/lexml/acao/removerElementoAction';
@@ -19,6 +25,18 @@ export const removeElemento = (state: any, action: any): State => {
     return state;
   }
 
+  if (isAgrupador(dispositivo) && !isDispositivoAlteracao(dispositivo)) {
+    // Só deixa remover agrupador se articulação permenecer consistente
+    if (isArticulacao(dispositivo.pai!)) {
+      const tipos = getTiposAgrupadorArtigoPermitidosNaArticulacao();
+      if (!dispositivo.filhos.every(f => isArtigo(f) || tipos.includes(f.tipo))) {
+        return retornaEstadoAtualComMensagem(state, { tipo: TipoMensagem.ERROR, descricao: 'Operação não permitida' });
+      }
+    } else if (dispositivo.filhos.filter(f => !isArtigo(f)).length) {
+      return retornaEstadoAtualComMensagem(state, { tipo: TipoMensagem.ERROR, descricao: 'Operação não permitida' });
+    }
+  }
+
   if (!isAcaoPermitida(dispositivo, RemoverElemento)) {
     return retornaEstadoAtualComMensagem(state, { tipo: TipoMensagem.ERROR, descricao: 'Não é possível excluir um dispositivo original mas apenas suprimi-lo.' });
   }
@@ -31,12 +49,18 @@ export const removeElemento = (state: any, action: any): State => {
   }
 
   const primeiroFilhoDoAgrupador = isAgrupador(dispositivo) ? dispositivo.filhos[0] || getDispositivoPosterior(dispositivo) || getDispositivoAnterior(dispositivo) : undefined;
+  const elPrimeiroFilhoDoAgrupador = primeiroFilhoDoAgrupador ? createElemento(primeiroFilhoDoAgrupador) : undefined;
+  const isAtualizarElementoEmenta = hasEmenta(dispositivo) && dispositivo === getPrimeiroAgrupadorNaArticulacao(dispositivo);
 
   const events = isAgrupador(dispositivo) ? removeAgrupadorAndBuildEvents(state.articulacao, dispositivo) : removeAndBuildEvents(state.articulacao, dispositivo);
 
-  if (primeiroFilhoDoAgrupador) {
-    events.push({ stateType: StateType.ElementoMarcado, elementos: [createElemento(primeiroFilhoDoAgrupador)] });
-    events.push({ stateType: StateType.ElementoReferenciado, elementos: [createElemento(primeiroFilhoDoAgrupador)] });
+  if (elPrimeiroFilhoDoAgrupador) {
+    events.push({ stateType: StateType.ElementoMarcado, elementos: [elPrimeiroFilhoDoAgrupador] });
+    events.push({ stateType: StateType.ElementoReferenciado, elementos: [elPrimeiroFilhoDoAgrupador] });
+  }
+
+  if (isAtualizarElementoEmenta) {
+    events.push({ stateType: StateType.SituacaoElementoModificada, elementos: [createElemento(state.articulacao.projetoNorma.ementa)] });
   }
 
   return {
