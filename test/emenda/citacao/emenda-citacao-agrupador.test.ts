9b174760b5572bfa9326f9ab0287378706c8c1a6
Marcos Fragomeni <marcos.fragomeni@gmail.com>
2023-03-06 15:36:43 -0300

    test: cmd emd alteração denominação de agrupador
    refs #530

---
+++ test/emenda/citacao/emenda-citacao-agrupador.test.ts
@@ -0,0 +1,37 @@
+import { expect } from '@open-wc/testing';
+import { CitacaoComandoDispPrj } from '../../../src/emenda/citacao-cmd-disp-prj';
+
+import { buildProjetoNormaFromJsonix } from '../../../src/model/lexml/documento/conversor/buildProjetoNormaFromJsonix';
+import { ProjetoNorma } from '../../../src/model/lexml/documento/projetoNorma';
+import { State, DefaultState } from '../../../src/redux/state';
+import { TesteCmdEmdUtil } from '../teste-cmd-emd-util';
+
+import { PLC_ARTIGOS_AGRUPADOS } from '../../doc/parser/plc_artigos_agrupados';
+
+let documento: ProjetoNorma;
+const state: State = new DefaultState();
+
+describe('Citação envolvendo alterações de agrupadores de artigo', () => {
+  beforeEach(function () {
+    documento = buildProjetoNormaFromJsonix(PLC_ARTIGOS_AGRUPADOS, true);
+    state.articulacao = documento.articulacao;
+  });
+
+  it('Deveria possuir 9 artigos', () => {
+    expect(state.articulacao?.artigos.length).to.equal(9);
+  });
+
+  // Símbolos: § “a”
+
+  // --------------------------------------------------------------------------------
+  // Alteração de denominação
+
+  it('Altera denominação de capítulo', () => {
+    /*
+     * 'Art. 1º-1. nononoono'
+     */
+    TesteCmdEmdUtil.modificaDispositivo(state, 'cap1');
+    const cit = new CitacaoComandoDispPrj(state.articulacao!).getTexto();
+    expect(cit).to.equal('<p class="agrupador">“<Rotulo>CAPÍTULO I</Rotulo></p><p class="agrupador"> Texto”</p>');
+  });
+});
