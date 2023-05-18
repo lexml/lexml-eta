3895da8a01de891769f896d11a53b99d3d0a09cc
Dennys Marinho <dennysmar@gmail.com>
2023-05-18 19:51:57 -0300

    fix: Validar dispositivo pai após suprimir
    Validar dispositivo pai após suprimir ou desfazer supressão de incisos, alíneas ou itens.
    
    refs: #576

--- src/redux/elemento/evento/eventosUtil.ts
+++ src/redux/elemento/evento/eventosUtil.ts
@@ -1,5 +1,5 @@
 import { Artigo } from './../../../model/dispositivo/dispositivo';
-import { hasFilhos, getAgrupadorAntes } from './../../../model/lexml/hierarquia/hierarquiaUtil';
+import { hasFilhos, getAgrupadorAntes, getArtigo } from './../../../model/lexml/hierarquia/hierarquiaUtil';
 import { Articulacao, Dispositivo } from '../../../model/dispositivo/dispositivo';
 import { DescricaoSituacao } from '../../../model/dispositivo/situacao';
 import { isAgrupador, isArticulacao, isArtigo, isCaput } from '../../../model/dispositivo/tipo';
@@ -257,8 +257,14 @@ export const suprimeAndBuildEvents = (articulacao: Articulacao, dispositivo: Dis
     dispositivo.alteracoes.filhos.forEach(f => getDispositivoAndFilhosAsLista(f).forEach(d => (d.situacao = new DispositivoSuprimido(createElemento(d)))));
   }
   const eventos = new Eventos();
-
   eventos.add(StateType.ElementoSuprimido, getElementos(dispositivo));
+
+  const artigo = getArtigo(dispositivo);
+  if (artigo) {
+    artigo.mensagens = validaDispositivo(artigo);
+    const elementoArtigo = createElemento(artigo, true);
+    eventos.add(StateType.ElementoValidado, [elementoArtigo]);
+  }
 
   return eventos.build();
 };
