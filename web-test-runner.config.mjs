0930115c3fa005f341b7f7f2ad3e4762e784ee69
fecocao <filipeedusoares@gmail.com>
2023-07-11 14:28:42 -0300

    fix: tirando comentário web-test-runner

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -2,10 +2,10 @@
 
 export default /** @type {import("@web/test-runner").TestRunnerConfig} */ ({
   files: [
-    //'out-tsc/test/**/*.test.js',
-    //'!out-tsc/test/emenda/**/*.test.js',    
+    'out-tsc/test/**/*.test.js',
+    '!out-tsc/test/emenda/**/*.test.js',    
     //'out-tsc/test/redux/colar/**/*.test.js',
-    'out-tsc/test/redux/aplicaEmenda/**/*.test.js',
+    //'out-tsc/test/redux/aplicaEmenda/**/*.test.js',
   ],
   nodeResolve: true,
 
