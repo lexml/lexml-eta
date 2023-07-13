3ba949c775cc2210c72fc3c691c9924a631dd5e6
fecocao <filipeedusoares@gmail.com>
2023-07-13 09:39:32 -0300

    test: criação de testes unitários - colar substituindo e verificando supressão dos disps originais na colagem

---
+++ test/redux/colar/reducer-colar-dispositivo-006.test.ts
@@ -0,0 +1,57 @@
+import { buscaDispositivoById, isDispositivoAlteracao } from '../../../src/model/lexml/hierarquia/hierarquiaUtil';
+import { adicionaElementosNaProposicaoFromClipboard } from '../../../src/redux/elemento/reducer/adicionaElementosNaProposicaoFromClipboard';
+import { State } from '../../../src/redux/state';
+import { MPV_905_2019 } from '../../doc/mpv_905_2019';
+import { expect } from '@open-wc/testing';
+import { buildProjetoNormaFromJsonix } from '../../../src/model/lexml/documento/conversor/buildProjetoNormaFromJsonix';
+import { openArticulacaoAction } from '../../../src/model/lexml/acao/openArticulacaoAction';
+import { ADICIONAR_ELEMENTOS_FROM_CLIPBOARD } from '../../../src/model/lexml/acao/AdicionarElementosFromClipboardAction';
+import { createElemento } from '../../../src/model/elemento/elementoUtil';
+import { TEXTO_006 } from '../../doc/textos-colar/texto_006';
+
+let state: State;
+
+describe('Testando carregamento da MPV 905/2019', () => {
+  beforeEach(function () {
+    const projetoNorma = buildProjetoNormaFromJsonix(MPV_905_2019, true);
+    state = openArticulacaoAction(projetoNorma.articulacao!);
+    state.ui = {} as any;
+  });
+
+  it('Deveria possuir articulação com 7 capítulos', () => {
+    expect(state.articulacao?.filhos.length).to.equal(7);
+    expect(state.articulacao?.filhos.every(f => f.tipo === 'Capitulo')).to.be.true;
+  });
+
+  it('Deveria possuir articulação com 53 artigos', () => {
+    expect(state.articulacao?.artigos.length).to.equal(53);
+  });
+
+  describe('Testando aplicação de dispositivos emendados', () => {
+    beforeEach(function () {
+      const disp = buscaDispositivoById(state.articulacao!, 'art1')!;
+      const atual = createElemento(disp);
+      const isColarSubstituindo = true;
+      state = adicionaElementosNaProposicaoFromClipboard(state, {
+        type: ADICIONAR_ELEMENTOS_FROM_CLIPBOARD,
+        atual,
+        novo: {
+          isDispositivoAlteracao: isDispositivoAlteracao(disp),
+          conteudo: {
+            texto: TEXTO_006,
+          },
+        },
+        isColarSubstituindo,
+        posicao: 'depois',
+      });
+    });
+
+    it('Deveria possuir parágrafos do art 16 com situação Dispositivo Suprimido', () => {
+      expect(state.articulacao!.artigos[15].filhos.every(a => a.situacao.descricaoSituacao === 'Dispositivo Suprimido')).to.equal(true);
+    });
+
+    it('Deveria possuir parágrafos do art 19 com situação Dispositivo Suprimido', () => {
+      expect(state.articulacao!.artigos[18].filhos.every(a => a.situacao.descricaoSituacao === 'Dispositivo Suprimido')).to.equal(true);
+    });
+  });
+});
