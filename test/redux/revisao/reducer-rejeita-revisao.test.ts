9772be68c000bc0243ff49a2f688400fc7d2137b
robsonbarrosdf <robsonbarrosdf@gmail.com>
2023-07-17 20:19:27 -0300

    test: testando rejeição de todas as revisões de uma só vez

--- test/redux/revisao/reducer-rejeita-revisao.test.ts
+++ test/redux/revisao/reducer-rejeita-revisao.test.ts
@@ -4,7 +4,7 @@ import { REJEITAR_REVISAO } from './../../../src/model/lexml/acao/rejeitarRevisa
 import { isArticulacao } from '../../../src/model/dispositivo/tipo';
 import { getDispositivoAndFilhosAsLista, isAdicionado, isSuprimido, buscaDispositivoById } from '../../../src/model/lexml/hierarquia/hierarquiaUtil';
 import { isRevisaoPrincipal, findRevisaoByElementoLexmlId, findRevisaoByElementoUuid2, findRevisaoById } from '../../../src/redux/elemento/util/revisaoUtil';
-import { State, StateType } from '../../../src/redux/state';
+import { State, StateEvent, StateType } from '../../../src/redux/state';
 import { MPV_905_2019 } from '../../doc/mpv_905_2019';
 import { expect } from '@open-wc/testing';
 import { buildProjetoNormaFromJsonix } from '../../../src/model/lexml/documento/conversor/buildProjetoNormaFromJsonix';
@@ -23,6 +23,7 @@ import { EMENDA_005 } from '../../doc/emendas/emenda-005';
 import { REDO } from '../../../src/model/lexml/acao/redoAction';
 import { MOVER_ELEMENTO_ABAIXO } from '../../../src/model/lexml/acao/moverElementoAbaixoAction';
 import { elementoSelecionadoAction } from '../../../src/model/lexml/acao/elementoSelecionadoAction';
+import { EMENDA_006 } from '../../doc/emendas/emenda-006';
 
 let state: State;
 let aux: any;
@@ -42,6 +43,108 @@ describe('Carregando texto da MPV 905/2019', () => {
 
     it('Deveria estar em revisão', () => {
       expect(state.emRevisao).to.be.true;
+    });
+
+    describe('Testando rejeição de múltiplas revisões (*)', () => {
+      beforeEach(function () {
+        state = elementoReducer(state, { type: APLICAR_ALTERACOES_EMENDA, alteracoesEmenda: EMENDA_006.componentes[0].dispositivos });
+
+        // gera 1 revisão
+        state = elementoReducer(state, { type: SUPRIMIR_ELEMENTO, atual: createElemento(buscaDispositivoById(state.articulacao!, 'art2_par1')!) });
+
+        // gera 1 revisão
+        state = elementoReducer(state, { type: SUPRIMIR_ELEMENTO, atual: createElemento(buscaDispositivoById(state.articulacao!, 'art2_par3')!) });
+
+        // gera 2 revisões (Art. 3º possui parágrafo único)
+        state = elementoReducer(state, { type: SUPRIMIR_ELEMENTO, atual: createElemento(buscaDispositivoById(state.articulacao!, 'art3')!) });
+
+        // gerar 3 revisões (o inciso movimentado possui 2 alíneas)
+        state = elementoReducer(state, { type: MOVER_ELEMENTO_ABAIXO, atual: createElemento(buscaDispositivoById(state.articulacao!, 'art1_par1u_inc1-1')!) });
+
+        // gerar 3 revisões (o inciso removido possui 2 alíneas)
+        state = elementoReducer(state, { type: REMOVER_ELEMENTO, atual: createElemento(buscaDispositivoById(state.articulacao!, 'art1_par1u_inc1-3')!) });
+      });
+
+      it('Deveria possuir 10 revisões, sendo 5 principais', () => {
+        expect(state.revisoes?.length).to.be.equal(10);
+        expect(state.revisoes?.filter(isRevisaoPrincipal).length).to.be.equal(5);
+      });
+
+      describe('Rejeitando todas as revisões', () => {
+        beforeEach(function () {
+          state = elementoReducer(state, { type: REJEITAR_REVISAO });
+        });
+
+        it('Deveria não possuir revisão', () => {
+          expect(state.revisoes?.length).to.be.equal(0);
+        });
+
+        it('Deveria possuir 6 itens em State.past', () => {
+          expect(state.past?.length).to.be.equal(6);
+        });
+
+        it('State.past deveria possuir 5 eventos RevisaoRejeitada', () => {
+          const eventos = (state.past![5] as unknown as StateEvent[]).filter(ev => ev.stateType === StateType.RevisaoRejeitada);
+          expect(eventos.length).to.be.equal(5);
+        });
+
+        it('Deveria possuir 18 dispositivos adicionados', () => {
+          const dispositivos = getDispositivoAndFilhosAsLista(state.articulacao!).filter(d => !isArticulacao(d) && isAdicionado(d));
+          expect(dispositivos.length).to.be.equal(18);
+        });
+
+        it('Deveria não possuir dispositivo suprimido', () => {
+          const dispositivos = getDispositivoAndFilhosAsLista(state.articulacao!).filter(isSuprimido);
+          expect(dispositivos.length).to.be.equal(0);
+        });
+
+        // it('State.past deveria possuir 1 evento ElementoValidado', () => {
+        //   const eventos = (state.past![5] as unknown as StateEvent[]).filter(ev => ev.stateType === StateType.ElementoValidado);
+        //   expect(eventos.length).to.be.equal(1);
+        //   expect(eventos[0].elementos?.length).to.be.equal(7); // Os dispositivos removidos não são validados
+        // });
+
+        describe('Fazendo UNDO da rejeição', () => {
+          beforeEach(function () {
+            state = elementoReducer(state, { type: UNDO });
+          });
+
+          it('Deveria possuir 10 revisões, sendo 5 principais', () => {
+            expect(state.revisoes?.length).to.be.equal(10);
+            expect(state.revisoes?.filter(isRevisaoPrincipal).length).to.be.equal(5);
+          });
+
+          it('Deveria possuir 15 dispositivos adicionados', () => {
+            const dispositivos = getDispositivoAndFilhosAsLista(state.articulacao!).filter(d => !isArticulacao(d) && isAdicionado(d));
+            expect(dispositivos.length).to.be.equal(15);
+          });
+
+          it('Deveria possuir 4 dispositivos suprimidos', () => {
+            const dispositivos = getDispositivoAndFilhosAsLista(state.articulacao!).filter(isSuprimido);
+            expect(dispositivos.length).to.be.equal(4);
+          });
+
+          describe('Fazendo REDO da rejeição', () => {
+            beforeEach(function () {
+              state = elementoReducer(state, { type: REDO });
+            });
+
+            it('Deveria não possuir revisão', () => {
+              expect(state.revisoes?.length).to.be.equal(0);
+            });
+
+            it('Deveria possuir 18 dispositivos adicionados', () => {
+              const dispositivos = getDispositivoAndFilhosAsLista(state.articulacao!).filter(d => !isArticulacao(d) && isAdicionado(d));
+              expect(dispositivos.length).to.be.equal(18);
+            });
+
+            it('Deveria não possuir dispositivo suprimido', () => {
+              const dispositivos = getDispositivoAndFilhosAsLista(state.articulacao!).filter(isSuprimido);
+              expect(dispositivos.length).to.be.equal(0);
+            });
+          });
+        });
+      });
     });
 
     describe('Suprimindo "art1_par1u_inc1"', () => {
