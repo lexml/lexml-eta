4b6f76e6d8aa5dc1ecb68dd6ad21e63d04eb2e61
Robson Barros <robsonbarrosdf@gmail.com>
2023-07-17 10:58:50 -0300

    feat: adicionando função para retornar diferença entre 2 textos (usa lib "diff")

--- src/util/string-util.ts
+++ src/util/string-util.ts
@@ -1,3 +1,5 @@
+import { diffChars, diffWords } from 'diff';
+
 export function containsTags(text: string): boolean {
   return /<.+>/g.test(text?.trim());
 }
@@ -125,4 +127,13 @@ export const getIniciais = (texto = ''): string => {
       .filter((_, i, arr) => i === 0 || i === arr.length - 1)
       .join('') || ''
   );
+};
+
+export const textoDiffAsHtml = (texto1: string, texto2: string, typeDiff: 'diffChars' | 'diffWords'): string => {
+  const fn = typeDiff === 'diffChars' ? diffChars : diffWords;
+  const buildPartAdded = (str: string): string => `<ins>${str}</ins>`; //`<span class="texto-inserido">${str}</span>`;
+  const buildPartRemoved = (str: string): string => `<del>${str}</del>`; //`<span classs="texto-removido">${str}</span>`;
+
+  const diff = fn(texto1, texto2);
+  return diff.map(part => (part.added ? buildPartAdded(part.value) : part.removed ? buildPartRemoved(part.value) : part.value)).join('');
 };
