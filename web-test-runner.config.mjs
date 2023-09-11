c52df4c60e526dea29aa29397755c46c4c6a3bce
robsonbarrosdf <robsonbarrosdf@gmail.com>
2023-09-11 19:28:32 -0300

    refactor: alterando web-test-runner.config.mjs

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -1,5 +1,47 @@
 // import { playwrightLauncher } from '@web/test-runner-playwright';
 
+export default /** @type {import("@web/test-runner").TestRunnerConfig} */ ({
+  files: [
+    // 'out-tsc/test/**/*.test.js',
+    // '!out-tsc/test/emenda/**/*.test.js',
+    //'out-tsc/test/redux/colar/**/*.test.js',
+    //'out-tsc/test/redux/aplicaEmenda/**/*.test.js',
+    'out-tsc/test/componente/editor-texto-rico/**/*.test.js',
+  ],
+  nodeResolve: true,
+
+  /** Compile JS for older browsers. Requires @web/dev-server-esbuild plugin */
+  // esbuildTarget: 'auto',
+
+  /** Confgure bare import resolve plugin */
+  // nodeResolve: {
+  //   exportConditions: ['browser', 'development']
+  // },
+
+  /** Amount of browsers to run concurrently */
+  // concurrentBrowsers: 2,
+
+  /** Amount of test files per browser to test concurrently */
+  // concurrency: 1,
+
+  /** Browsers to run tests on */
+  // browsers: [
+  //   playwrightLauncher({ product: 'chromium' }),
+  //   playwrightLauncher({ product: 'firefox' }),
+  //   playwrightLauncher({ product: 'webkit' }),
+  // ],
+
+  // See documentation for all available options
+  testRunnerHtml: testFramework =>
+    `<html>
+      ${css}
+      <body>
+        <script>window.process = { env: { NODE_ENV: "development", testMode: true } }</script>
+        <script type="module" src="${testFramework}"></script>
+      </body>
+    </html>`,
+});
+
 const css = `
 <style>
 .ql-container {
@@ -1409,45 +1451,3 @@ revisao-container {
 }
 </style>
 `;
-
-export default /** @type {import("@web/test-runner").TestRunnerConfig} */ ({
-  files: [
-    'out-tsc/test/**/*.test.js',
-    '!out-tsc/test/emenda/**/*.test.js',
-    //'out-tsc/test/redux/colar/**/*.test.js',
-    //'out-tsc/test/redux/aplicaEmenda/**/*.test.js',
-    // 'out-tsc/test/componente/editor-texto-rico/**/*.test.js',
-  ],
-  nodeResolve: true,
-
-  /** Compile JS for older browsers. Requires @web/dev-server-esbuild plugin */
-  // esbuildTarget: 'auto',
-
-  /** Confgure bare import resolve plugin */
-  // nodeResolve: {
-  //   exportConditions: ['browser', 'development']
-  // },
-
-  /** Amount of browsers to run concurrently */
-  // concurrentBrowsers: 2,
-
-  /** Amount of test files per browser to test concurrently */
-  // concurrency: 1,
-
-  /** Browsers to run tests on */
-  // browsers: [
-  //   playwrightLauncher({ product: 'chromium' }),
-  //   playwrightLauncher({ product: 'firefox' }),
-  //   playwrightLauncher({ product: 'webkit' }),
-  // ],
-
-  // See documentation for all available options
-  testRunnerHtml: testFramework =>
-    `<html>
-      ${css}
-      <body>
-        <script>window.process = { env: { NODE_ENV: "development", testMode: true } }</script>
-        <script type="module" src="${testFramework}"></script>
-      </body>
-    </html>`,
-});
