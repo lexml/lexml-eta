f3bb0ce31078fedcf3c36ed04b3bee8c818c557c
robsonbarrosdf <robsonbarrosdf@gmail.com>
2023-09-21 10:13:52 -0300

    refactor: usando classes para remover recuo de primeira linha e espaço após parágrafo

--- src/components/editor-texto-rico/text-indent.ts
+++ src/components/editor-texto-rico/text-indent.ts
@@ -1,26 +1,10 @@
-import { AttributorOptions } from 'parchment/dist/src/attributor/attributor';
-
-//import Parchment from 'parchment';
 const Parchment: any = Quill.import('parchment');
-
-class NoIndentAttributor extends Parchment.Attributor.Style {
-  constructor(attrName: string, keyName: string, options?: AttributorOptions) {
-    super(attrName, keyName, options);
-  }
-
-  add(node: HTMLElement, value: string): boolean {
-    if (!this.value(node)) {
-      return super.add(node, value);
-    }
-    this.remove(node);
-    return true;
-  }
-}
 
 const config = {
   scope: Parchment.Scope.BLOCK,
   whitelist: ['0px'],
 };
 
-const NoIndentClass = new NoIndentAttributor('text-indent', 'text-indent', config);
+// const NoIndentStyle = new Parchment.Attributor.Style('text-indent', 'text-indent', config);
+const NoIndentClass = new Parchment.Attributor.Class('text-indent', 'ql-text-indent', config);
 export { NoIndentClass };
