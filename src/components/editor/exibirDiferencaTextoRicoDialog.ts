0b801365385534d945a0dfaf5cc21909095b5868
robsonbarrosdf <robsonbarrosdf@gmail.com>
2023-09-26 20:07:32 -0300

    refactor: diferença entre texto original e texto revisado (editor-texto-rico)

--- src/components/editor/exibirDiferencaTextoRicoDialog.ts
+++ src/components/editor/exibirDiferencaTextoRicoDialog.ts
@@ -1,6 +1,5 @@
-import { ajustaHtmlParaColagem } from '../../redux/elemento/util/colarUtil';
 import { EtaQuill } from '../../util/eta-quill/eta-quill';
-import { textoDiffAsHtml } from '../../util/string-util';
+import { substituiEspacosPorNbsp, textoDiffAsHtml } from '../../util/string-util';
 
 export class TextoRicoDiff {
   quill!: EtaQuill;
@@ -17,8 +16,7 @@ export const exibirDiferencasTextoRicoDialog = (diff: TextoRicoDiff): void => {
   document.body.appendChild(dialogElem);
   dialogElem.label = 'Exibir diferenças após revisão';
 
-  diff.textoAntesRevisao = ajustaHtmlParaColagem(diff.textoAntesRevisao);
-  diff.textoAtual = ajustaHtmlParaColagem(diff.textoAtual);
+  const diferencas = diff.textoAntesRevisao !== diff.textoAtual ? calcDiferenca(diff.textoAntesRevisao, diff.textoAtual) : `<p>Não existe diferença</p>`;
 
   const fnDestroy = (): void => {
     try {
@@ -47,10 +45,16 @@ export const exibirDiferencasTextoRicoDialog = (diff: TextoRicoDiff): void => {
 
     .texto-alterado ins {
       background-color: #c6ffc6;
+      text-decoration: none;
     }
 
     .texto-alterado del {
       background-color: #ffc6c6;
+    }
+
+    .texto-alterado table {
+      border-collapse: collapse;
+      width: 100%;
     }
 
     sl-tab-panel::part(base) {
@@ -87,7 +91,7 @@ export const exibirDiferencasTextoRicoDialog = (diff: TextoRicoDiff): void => {
   </style>
   <div class="texto-alterado-texto-rico">
     <div class="texto-alterado">
-    ${diff.textoAntesRevisao !== diff.textoAtual ? textoDiffAsHtml(diff.textoAntesRevisao, diff.textoAtual, 'diffWords') : `<p>Não existe diferença</p>`}
+    ${diferencas}
     </div>
   </div>
 
@@ -102,4 +106,48 @@ export const exibirDiferencasTextoRicoDialog = (diff: TextoRicoDiff): void => {
   diff.quill && diff.quill.blur();
   dialogElem.appendChild(content);
   dialogElem.show();
+};
+
+const getOcorrenciasAberturaTag = (tag: string, texto: string): string[] => {
+  const regexAberturaTag = new RegExp(`<${tag}\\b[^>]*>`, 'gi');
+  return texto.match(regexAberturaTag) || [];
+};
+
+const transformatTagP = (texto: string): string => {
+  const regexAberturaTag = new RegExp('<p\\b[^>]*>', 'gi');
+  const regexFechamentoTag = new RegExp('</p>', 'gi');
+  return texto.replace(regexAberturaTag, () => `\n`).replace(regexFechamentoTag, ``);
+};
+
+export const calcDiferenca = (textoAntesRevisaoOriginal: string, textoAtualOriginal: string): string => {
+  // Remove as tags <br> do texto
+  const regexMatchBR = /<br\b[^>]*>/gi;
+  let textoAntesRevisao = textoAntesRevisaoOriginal.replace(regexMatchBR, '');
+  let textoAtual = textoAtualOriginal.replace(regexMatchBR, '');
+
+  // Identifica as aberturas de tags <p>
+  const ocorrenciasTagPTextoAtual = getOcorrenciasAberturaTag('p', textoAtual);
+
+  // Substitui as tags <p> por "\n" para que "textoDiffAsHtml (diffWords)" funcione corretamente
+  textoAntesRevisao = transformatTagP(textoAntesRevisao);
+  textoAtual = transformatTagP(textoAtual);
+
+  // Calcula a diferença entre os textos
+  let diferencas = textoDiffAsHtml(textoAntesRevisao, textoAtual, 'diffWords');
+  if (!/<ins>|<del>/.test(diferencas)) {
+    diferencas = substituiEspacosPorNbsp(textoDiffAsHtml(textoAntesRevisao, textoAtual, 'diffChars'), ['ins', 'del']);
+  }
+
+  // Ajusta as tags <ins> e <del> para que fiquem "dentro" das tags <p>
+  diferencas = diferencas
+    .replace(/\n<\/ins>/g, '</ins>\n')
+    .replace(/<ins>\n/g, '\n<ins>')
+    .replace(/\n<\/del>/g, '</del>\n')
+    .replace(/<del>\n/g, '\n<del>');
+
+  // Substitui "\n" pelas ocorrencias de abertura de tags <p>
+  diferencas = ocorrenciasTagPTextoAtual.reduce((acc, p, index) => acc.replace(/\n/, index ? '</p>' + p : p), diferencas) + '</p>';
+
+  // Substitui inconsistências no fechamento de tags
+  return diferencas.replace(/>>/g, '>');
 };
