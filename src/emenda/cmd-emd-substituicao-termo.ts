dadeeaa42435dd2cd711ce0aa136709b4fb43957
robsonbarrosdf <robsonbarrosdf@gmail.com>
2023-10-19 15:33:37 -0300

    fix: comando da emenda de substituição de termo

--- src/emenda/cmd-emd-substituicao-termo.ts
+++ src/emenda/cmd-emd-substituicao-termo.ts
@@ -7,11 +7,11 @@ export class CmdEmdSubstituicaoTermo {
     const flexoes: string[] = [];
     flexaoGenero && flexoes.push('gênero');
     flexaoNumero && flexoes.push('número');
-    return flexoes.length ? `", fazendo-se as flexões de ${flexoes.join(' e ')} necessárias` : '';
+    return flexoes.length ? `, fazendo-se as flexões de ${flexoes.join(' e ')} necessárias` : '';
   }
 
   getTexto(): string {
     const { tipo, termo, novoTermo, flexaoGenero, flexaoNumero } = this.substituicaoTermo;
-    return `Substitua-se na Medida Provisória a/o ${tipo.toLowerCase()} "${termo}" por "${novoTermo}"${this.getComplementoFlexoes(flexaoGenero, flexaoNumero)}.`;
+    return `Substitua-se na Medida Provisória a/o ${tipo.toLowerCase()} “${termo}” por “${novoTermo}”${this.getComplementoFlexoes(flexaoGenero, flexaoNumero)}.`;
   }
 }
