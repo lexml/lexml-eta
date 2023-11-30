e428590bca0da781fa6e752afb450b122d87222a
arthurcordeiro <arthur.cordeiro@gmail.com>
2023-11-30 14:22:24 -0300

    feat: converte modal nota de rodapé para shadow root e remove listeners ao fechar

--- src/components/editor-texto-rico/editor-texto-rico.component.ts
+++ src/components/editor-texto-rico/editor-texto-rico.component.ts
@@ -172,15 +172,7 @@ export class EditorTextoRicoComponent extends connect(rootStore)(LitElement) {
   private renderBotaoNotaRodape(): TemplateResult {
     return html`
       <div class="nota-rodape">
-        <button type="button" style="width:auto" title="Anexo" @click=${(): any => this.mostrarDialogNotaRodape()}>
-          <span>
-            <svg xmlns="http://www.w3.org/2000/svg" width="15px" height="15px" viewBox="0 0 35 35" data-name="Layer 2" id="Layer_2">
-              <path
-                d="M18,34.75A11.32,11.32,0,0,1,6.69,23.45V8A7.78,7.78,0,0,1,22.25,8V22.49a4.58,4.58,0,1,1-9.15,0V9.29a1.25,1.25,0,0,1,2.5,0v13.2a2.08,2.08,0,1,0,4.15,0V8A5.28,5.28,0,0,0,9.19,8V23.45A8.82,8.82,0,0,0,18,32.25c4.6,0,7.81-3.62,7.81-8.8V9.66a1.25,1.25,0,0,1,2.5,0V23.45C28.31,30,24,34.75,18,34.75Z"
-              />
-            </svg>
-          </span>
-        </button>
+        <button type="button" style="width:auto" title="Anexo" @click=${(): any => this.mostrarDialogNotaRodape()}>Modal nota de rodapé</button>
       </div>
     `;
   }
--- src/components/editor-texto-rico/nota-rodape-modal.ts
+++ src/components/editor-texto-rico/nota-rodape-modal.ts
@@ -1,13 +1,99 @@
 export class NotaRodapeModal {
   private modalElement: HTMLElement;
   private overlayElement: HTMLElement;
+  private shadowRoot: ShadowRoot;
+  private keydownListener: (event: KeyboardEvent) => void;
 
   constructor() {
     this.modalElement = document.createElement('div');
     this.modalElement.classList.add('modal');
-    this.modalElement.setAttribute('role', 'dialog');
-    this.modalElement.setAttribute('aria-labelledby', 'modalTitle');
-    this.modalElement.innerHTML = `
+    this.shadowRoot = this.modalElement.attachShadow({ mode: 'open' });
+
+    this.shadowRoot.innerHTML = `
+      <style>
+
+        :host {
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          transform: translate(-50%, -50%);
+          min-width: 400px;
+          max-width: 640px;
+          background-color: white;
+          padding: 20px;
+          border-radius: 10px;
+          box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+          z-index: 1010;
+          display: none;
+          flex-direction: column;
+          gap: 20px;
+        }
+
+        .modal-header {
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+        }
+
+        .modal-title {
+          margin: 0;
+          font-size: 20px;
+        }
+
+        .modal-footer {
+          display: flex;
+          gap: 10px;
+          justify-content: flex-end;
+        }
+
+        .modal-textarea {
+          width: 100%;
+          height: 100px;
+          margin-bottom: 15px;
+          font-family: inherit;
+          font-size: inherit;
+          padding: 10px;
+          box-sizing: border-box;
+        }
+
+        .modal-close-button {
+          cursor: pointer;
+          background-color: #eee;
+          padding: 5px 10px;
+          border-radius: 5px;
+          transition: background-color 0.3s;
+          border: none;
+        }
+
+        .header-close-button {
+          background-color: transparent;
+          font-size: 30px;
+        }
+
+        .modal-save-button {
+          cursor: pointer;
+          background-color: #0284c7;
+          color: white;
+          padding: 9px 14px;
+          border-radius: 5px;
+          border: none;
+        }
+
+        .modal-save-button:hover {
+          background-color: #0ea5e9;
+        }
+
+        .modal-close-button: hover {
+          background-color: #ddd;
+        }
+
+        @media (max-width: 600px) {
+          :host {
+            width: 80%;
+            min-width: 0;
+          }
+        }
+      </style>
       <div class="modal-header">
         <h1 id="modalTitle" class="modal-title">Editar nota de rodapé</h1>
         <button class="modal-close-button header-close-button" aria-label="Fechar" title="Fechar">&times;</button>
@@ -23,129 +109,52 @@ export class NotaRodapeModal {
 
     this.overlayElement = document.createElement('div');
     this.overlayElement.classList.add('overlay');
+    this.overlayElement.style.display = 'none';
+    this.overlayElement.style.position = 'fixed';
+    this.overlayElement.style.top = '0';
+    this.overlayElement.style.left = '0';
+    this.overlayElement.style.width = '100%';
+    this.overlayElement.style.height = '100%';
+    this.overlayElement.style.backgroundColor = 'rgba(0, 0, 0, 0.6)';
+    this.overlayElement.style.zIndex = '1000';
 
     document.body.appendChild(this.overlayElement);
     document.body.appendChild(this.modalElement);
 
-    Array.from(this.modalElement.getElementsByClassName('modal-close-button')).forEach(element => element.addEventListener('click', () => this.close()));
+    this.keydownListener = (event: KeyboardEvent) => {
+      if (event.key === 'Escape') {
+        this.close();
+      }
+    };
+
+    document.addEventListener('keydown', this.keydownListener);
+
+    Array.from(this.shadowRoot.querySelectorAll('.modal-close-button')).forEach(element => element.addEventListener('click', () => this.close()));
   }
 
-  // tranforma parametro content em opcional no metodo open
-
   open(content?: string): void {
-    setTimeout(() => {
-      const firstFocusableElement = this.modalElement.querySelector('.modal-textarea') as HTMLTextAreaElement;
-      if (firstFocusableElement) firstFocusableElement.focus();
-    }, 0);
-    const modalTitle = this.modalElement.querySelector('.modal-title');
+    this.modalElement.style.display = 'flex';
+    this.overlayElement.style.display = 'block';
+    const firstFocusableElement = this.shadowRoot.querySelector('.modal-textarea') as HTMLTextAreaElement;
+    if (firstFocusableElement) firstFocusableElement.focus();
+    const modalTitle = this.shadowRoot.querySelector('.modal-title');
     if (modalTitle) {
       modalTitle.innerHTML = content ? content : modalTitle.innerHTML;
     }
-    this.modalElement.style.display = 'flex';
-    this.overlayElement.style.display = 'block';
   }
 
   close(): void {
-    this.modalElement.style.display = 'none';
-    this.overlayElement.style.display = 'none';
-  }
-}
-
-const styles = `
-  .overlay {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(0, 0, 0, 0.6);
-    z-index: 1000;
-    display: none;
-  }
-
-  .modal {
-    position: fixed;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    min-width: 400px;
-    max-width: 640px;
-    background-color: white;
-    padding: 20px;
-    border-radius: 10px;
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-    z-index: 1010;
-    display: none;
-    flex-direction: column;
-    gap: 20px;
-  }
-
-  .modal-header {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
-
-  .modal-title {
-    margin: 0;
-    font-size: 20px;
-  }
-
-  .modal-footer {
-    display: flex;
-    gap: 10px;
-    justify-content: flex-end;
-  }
-
-  .modal-textarea {
-    width: 100%;
-    height: 100px;
-    margin-bottom: 15px;
-    font-family: inherit;
-    font-size: inherit;
-    padding: 10px;
-    box-sizing: border-box;
-  }
-
-  .modal-close-button {
-    cursor: pointer;
-    background-color: #eee;
-    padding: 5px 10px;
-    border-radius: 5px;
-    transition: background-color 0.3s;
-    border: none;
-  }
-
-  .header-close-button {
-    background-color: transparent;
-    font-size: 30px;
-  }
-
-  .modal-save-button {
-    cursor: pointer;
-    background-color: #0284c7;
-    color: white;
-    padding: 9px 14px;
-    border-radius: 5px;
-    border: none;
-  }
-
-  .modal-save-button:hover {
-    background-color: #0ea5e9;
-  }
-
-  .modal-close-button: hover {
-    background-color: #ddd;
-  }
-
-  @media (max-width: 600px) {
-    .modal {
-      width: 80%;
-      min-width: 0;
+    const textarea = this.shadowRoot.querySelector('.modal-textarea') as HTMLTextAreaElement;
+    if (textarea.value.length > 0) {
+      if (confirm('Tem certeza que deseja fechar? As alterações não salvas serão perdidas.')) {
+        document.removeEventListener('keydown', this.keydownListener);
+        this.modalElement.remove();
+        this.overlayElement.remove();
+      }
+    } else {
+      document.removeEventListener('keydown', this.keydownListener);
+      this.modalElement.remove();
+      this.overlayElement.remove();
     }
   }
-`;
-
-const styleSheet = document.createElement('style');
-styleSheet.innerText = styles;
-document.head.appendChild(styleSheet);
+}
