1557098367a14ecf86968994bb0a9a7cf64819b3
robsonbarrosdf <robsonbarrosdf@gmail.com>
2024-01-22 14:23:54 -0300

    feat: configuração cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,9 +2,28 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      const envName = config.env.envName || 'local';
+      const envFile = `./cypress/config/cypress.env.${envName}.json`;
+
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      const settings = require(envFile);
+
+      if (settings.baseUrl) {
+        config.baseUrl = settings.baseUrl;
+      }
+
+      if (settings.env) {
+        config.env = {
+          ...config.env,
+          ...settings.env,
+        };
+      }
+
+      return config;
     },
+    excludeSpecPattern: ['**/1-getting-started/**/*', '**/2-advanced-examples/**/*'],
+    experimentalStudio: true,
   },
 });
