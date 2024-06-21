42474edf4de4324f76dcb39249bf227d5def2c11
robsonbarrosdf <robsonbarrosdf@gmail.com>
2024-06-20 21:11:06 -0300

    feat: adicionando script para carregar web-dev-server usando cache

--- web-dev-server.config.mjs
+++ web-dev-server.config.mjs
@@ -1,7 +1,29 @@
 // import { hmrPlugin, presets } from '@open-wc/dev-server-hmr';
 
 /** Use Hot Module replacement by adding --hmr to the start command */
+
+function cacheMiddleware() {
+  console.log('Cache middleware enabled');
+  return async (context, next) => {
+    await next();
+    if (context.url.match(/\.(js|css|png|jpg|jpeg|gif|ico)$/)) {
+      context.response.set('Cache-Control', 'public, max-age=31536000');
+    }
+  };
+}
+
 const hmr = process.argv.includes('--hmr');
+const cacheEnabled = process.argv.includes('--cache');
+
+const middlewares = [
+  proxy('/api/', {
+    target: 'https://www6ghml.senado.leg.br/',
+    rewrite: path => path.replace(/^\/api/, '/editor-emendas/api'),
+    logs: true,
+    changeOrigin: true
+  }),
+];
+cacheEnabled && middlewares.push(cacheMiddleware());
 
 import proxy from 'koa-proxies';
 //import httpsProxyAgent from 'https-proxy-agent';
@@ -24,14 +46,7 @@ export default /** @type {import('@web/dev-server').DevServerConfig} */ ({
   // },
 
   // TODO Não está funcionando na rede do SF por não estar utilizando o proxy da rede no acesso à url externa
-  middleware: [
-    proxy('/api/', {
-      target: 'https://www6ghml.senado.leg.br/',
-      rewrite: path => path.replace(/^\/api/, '/editor-emendas/api'),
-      logs: true,
-      changeOrigin: true
-    }),
-  ],
+  middleware: middlewares,
 
   plugins: [
     /** Use Hot Module Replacement by uncommenting. Requires @open-wc/dev-server-hmr plugin */
