aff37291c9a38b7aea37b66a194dbf11ea71e670
robsonbarrosdf <robsonbarrosdf@gmail.com>
2024-06-25 15:53:59 -0300

    cy-test: novos testes Cypress de abertura de emendas

--- cypress/e2e/nova-emenda/nova-emenda-mpv-905.cy.ts
+++ cypress/e2e/nova-emenda/nova-emenda-mpv-905.cy.ts
@@ -1,30 +1,4 @@
 describe('Emenda para MPV 905', () => {
-  it('Emenda "artigo onde couber"', () => {
-    cy.novaEmenda({
-      projetoNormaSelectValue: 'mpv_905_2019',
-      modoEmendaSelectValue: 'emendaArtigoOndeCouber',
-    });
-
-    cy.checarEstadoInicialAoCriarNovaEmendaOndeCouber({ nomeProposicao: 'MPV 905/2019', totalElementos: 2 });
-
-    // cy.get('#texto__dispositivo3').then($el => {
-    //   // $el[0].textContent = 'AAA BBB CCC DDD EEE FFF GGG HHH';
-
-    //   // cy.get('lexml-eta-editor').then($eta => {
-    //   //   const eta = $eta[0];
-    //   //   (eta as any).emitirEventoOnChange('cypress');
-    //   // });
-
-    //   const $el0 = cy.wrap($el[0]);
-    //   $el0.type('PPP QQQ RRR SSS TTT UUU WWW XXX YYY ZZZ.', { force: true });
-    // });
-
-    const texto = 'Texto do artigo onde couber.';
-    cy.get('div.container__elemento.dispositivo--adicionado').should('have.length', 1).inserirTextoNoDispositivo(texto).contains(texto);
-    cy.checarComandoEmenda();
-    cy.checarTextoPresenteEmComandoEmenda(texto);
-  });
-
   it('Emenda padrão - inclusão de dispositivo', () => {
     cy.novaEmenda({
       projetoNormaSelectValue: 'mpv_905_2019',
@@ -52,6 +26,32 @@ describe('Emenda para MPV 905', () => {
     const texto = 'Novo texto do parágrafo 1º do artigo 2º.';
     cy.getContainerArtigoByNumero(2).next().click().should('have.length', 1).inserirTextoNoDispositivo(texto).contains(texto);
     // cy.checarComandoEmenda();
+    cy.checarTextoPresenteEmComandoEmenda(texto);
+  });
+
+  it('Emenda "artigo onde couber"', () => {
+    cy.novaEmenda({
+      projetoNormaSelectValue: 'mpv_905_2019',
+      modoEmendaSelectValue: 'emendaArtigoOndeCouber',
+    });
+
+    cy.checarEstadoInicialAoCriarNovaEmendaOndeCouber({ nomeProposicao: 'MPV 905/2019', totalElementos: 2 });
+
+    // cy.get('#texto__dispositivo3').then($el => {
+    //   // $el[0].textContent = 'AAA BBB CCC DDD EEE FFF GGG HHH';
+
+    //   // cy.get('lexml-eta-editor').then($eta => {
+    //   //   const eta = $eta[0];
+    //   //   (eta as any).emitirEventoOnChange('cypress');
+    //   // });
+
+    //   const $el0 = cy.wrap($el[0]);
+    //   $el0.type('PPP QQQ RRR SSS TTT UUU WWW XXX YYY ZZZ.', { force: true });
+    // });
+
+    const texto = 'Texto do artigo onde couber.';
+    cy.get('div.container__elemento.dispositivo--adicionado').should('have.length', 1).inserirTextoNoDispositivo(texto).contains(texto);
+    cy.checarComandoEmenda();
     cy.checarTextoPresenteEmComandoEmenda(texto);
   });
 });
