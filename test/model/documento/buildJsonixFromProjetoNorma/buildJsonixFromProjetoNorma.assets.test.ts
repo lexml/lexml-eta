6ada8c6e0d1b854bc444daf07dcf2e02f28468cd
João Matos <joaofmatos.10@gmail.com>
2026-02-13 22:20:28 -0300

    fix: Adiciona testes para o pls_547_2018.

--- test/model/documento/buildJsonixFromProjetoNorma/buildJsonixFromProjetoNorma.assets.test.ts
+++ test/model/documento/buildJsonixFromProjetoNorma/buildJsonixFromProjetoNorma.assets.test.ts
@@ -11,6 +11,7 @@ import { Plc_142_2018 } from '../../../assets/plc_142_2018';
 import { PL_5008_2023 } from '../../../assets/pl_5008_2023';
 import { PLP_68_2024 } from '../../../assets/plp_68_2024';
 import { PLP_197_2023 } from '../../../assets/plp_197_2023';
+import { PLS_547_2018 } from '../../../assets/pls_547_2018';
 import { validarRecursivo } from '../../../../demo/components/jsonValidator';
 import type { LogErro } from '../../../../demo/components/jsonValidator';
 import { filtrarErrosConteudoParagrafo, filtrarErrosLinksNomeAgrupador, filtrarErrosNotaAlteracaoOmissis, filtrarErrosPreambuloVazio } from './buildJsonixHelpers';
@@ -98,6 +99,12 @@ describe('buildJsonixFromProjetoNorma - Assets Integration Tests', () => {
   describe('PLP 197/2023', () => {
     it('Deveria gerar estrutura jsonix idêntica ao arquivo JSON esperado (ignorando conteúdo de parágrafos com elementos inline)', () => {
       testarConversaoJsonix(PLP_197_2023, 'PLP 197/2023', [filtrarErrosConteudoParagrafo]);
+    });
+  });
+
+  describe('PLS 547/2018', () => {
+    it('Deveria gerar estrutura jsonix idêntica ao arquivo JSON esperado (ignorando elementos vazios no preâmbulo)', () => {
+      testarConversaoJsonix(PLS_547_2018, 'PLS 547/2018', [filtrarErrosPreambuloVazio]);
     });
   });
 });
