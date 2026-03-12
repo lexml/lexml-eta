c8d611a70b7613e96e401cb772753e65838b408f
João Matos <joaofmatos.10@gmail.com>
2026-03-12 11:32:45 -0300

    fix: corrige problema com atualização de remissões manual.

--- src/model/lexml/regras/regrasArtigo.ts
+++ src/model/lexml/regras/regrasArtigo.ts
@@ -99,7 +99,7 @@ export function RegrasArtigo<TBase extends Constructor>(Base: TBase): any {
         acoes.push(InformarDadosAssistenteAction);
       }
 
-      if (isDispositivoAlteracao(dispositivo)) {
+      if (isDispositivoAlteracao(dispositivo) || dispositivo.situacao.descricaoSituacao === DescricaoSituacao.DISPOSITIVO_ADICIONADO) {
         acoes.push(renumerarElementoAction);
       }
       if (dispositivo.alteracoes && dispositivo.situacao.descricaoSituacao !== DescricaoSituacao.DISPOSITIVO_MODIFICADO) {
