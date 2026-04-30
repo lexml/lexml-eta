a265d6ada676de100d5e7a9b1f2525fa818328ec
João Matos <joaofmatos.10@gmail.com>
2026-04-30 11:54:52 -0300

    fix: Corrige erro na abertura de aspas do dispositivo.

--- src/model/elemento/elementoUtil.ts
+++ src/model/elemento/elementoUtil.ts
@@ -126,7 +126,7 @@ export const createElemento = (dispositivo: Dispositivo, acoes = true, procurarE
     acoesPossiveis: acoes ? dispositivo.getAcoesPossiveis(dispositivo) : [],
     descricaoSituacao: dispositivo.situacao?.descricaoSituacao,
     mensagens: isOriginal(dispositivo) && !isBloqueado(dispositivo) ? [] : dispositivo.mensagens,
-    abreAspas: isDispositivoCabecaAlteracao(dispositivo),
+    abreAspas: isDispositivoCabecaAlteracao(dispositivo) || !!dispositivo.cabecaAlteracao,
     fechaAspas,
     notaAlteracao,
     dispositivoAlteracao: isDispositivoAlteracao(dispositivo),
