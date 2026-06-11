55e884c369070da63efb547285cd809063f7728d
joaofreitas <joaofreitas@users.noreply.github.com>
2026-06-11 01:28:42 +0000

    Update gh-pages to output generated at a36cd66

---
+++ assets/icons/icons.ts
@@ -0,0 +1,62 @@
+export const negrito = `
+<svg class="icon-negrito" id="negrito" viewBox="0 0 16 16">
+<path class="ql-fill" d="M10.7,13.5c-0.1,0-0.4,0-0.6-0.2L6,7.8C6,7.7,5.8,7.7,5.8,7.8l0,5.1c0,0.3-0.2,0.5-0.5,0.5H3.7
+	c-0.3,0-0.5-0.2-0.5-0.5l0-9.5c-0.1-0.2,0-0.4,0.1-0.5c0.1-0.2,0.4-0.4,0.7-0.4h1c0.1,0,0.4,0,0.6,0.2l4.4,5.9
+	c0.1,0.1,0.2,0,0.2-0.1V3c0-0.3,0.2-0.5,0.5-0.5h1.5c0.3,0,0.5,0.2,0.5,0.5V13c0,0.3-0.2,0.5-0.5,0.5H10.7z"/>
+</svg>
+`;
+
+export const sublinhado = `
+<svg class="icon-sublinhado" id="sublinhado" viewBox="0 0 16 16">
+<path class="ql-fill" d="M4.3,9.4c0-0.2,0.1-0.3,0.4-0.3h0.7c0.2,0,0.4,0.1,0.4,0.3C5.8,10.4,6.7,11,8.1,11c1.3,0,2.2-0.5,2.2-1.4
+	c0-1.1-1.2-1.4-2.5-1.9C6.3,7.3,4.5,6.8,4.5,5c0-1.6,1.5-2.7,3.5-2.7c1.9,0,3.3,1.1,3.4,2.6c0,0.2-0.1,0.3-0.4,0.3h-0.7
+	c-0.2,0-0.3-0.1-0.4-0.3C9.9,4.1,9.2,3.7,8.1,3.7C6.8,3.7,6,4.2,6,5c0,0.9,0.9,1.2,2.2,1.6c1.6,0.5,3.5,1.1,3.5,3.1
+	c0,1.6-1.5,2.7-3.6,2.7C5.9,12.3,4.3,11.2,4.3,9.4z"/>
+<rect x="3" y="12.9" class="ql-fill" width="9.9" height="0.8"/>
+</svg>
+`;
+
+export const controleDropdown =
+  '<svg viewBox="0 0 18 18"> <polygon class="ql-stroke" points="7 11 9 13 11 11 7 11"></polygon> <polygon class="ql-stroke" points="7 7 9 5 11 7 7 7"></polygon> </svg>';
+
+export const anexo =
+  '<svg xmlns="http://www.w3.org/2000/svg" fill="#000000" width="15px" height="15px" viewBox="0 0 35 35" data-name="Layer 2" id="Layer_2"><path d="M18,34.75A11.32,11.32,0,0,1,6.69,23.45V8A7.78,7.78,0,0,1,22.25,8V22.49a4.58,4.58,0,1,1-9.15,0V9.29a1.25,1.25,0,0,1,2.5,0v13.2a2.08,2.08,0,1,0,4.15,0V8A5.28,5.28,0,0,0,9.19,8V23.45A8.82,8.82,0,0,0,18,32.25c4.6,0,7.81-3.62,7.81-8.8V9.66a1.25,1.25,0,0,1,2.5,0V23.45C28.31,30,24,34.75,18,34.75Z"/></svg>';
+export const lixeiro =
+  '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="15" height="15" viewBox="0 0 256 256" xml:space="preserve"><defs></defs><g style="stroke: none; stroke-width: 0; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: none; fill-rule: nonzero; opacity: 1;" transform="translate(1.4065934065934016 1.4065934065934016) scale(2.81 2.81)"><path d="M 76.777 2.881 H 57.333 V 2.412 C 57.333 1.08 56.253 0 54.921 0 H 35.079 c -1.332 0 -2.412 1.08 -2.412 2.412 v 0.469 H 13.223 c -1.332 0 -2.412 1.08 -2.412 2.412 v 9.526 c 0 1.332 1.08 2.412 2.412 2.412 h 63.554 c 1.332 0 2.412 -1.08 2.412 -2.412 V 5.293 C 79.189 3.961 78.109 2.881 76.777 2.881 z" style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(0,0,0); fill-rule: nonzero; opacity: 1;" transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round"/><path d="M 73.153 22.119 H 16.847 c -1.332 0 -2.412 1.08 -2.412 2.412 v 63.057 c 0 1.332 1.08 2.412 2.412 2.412 h 56.306 c 1.332 0 2.412 -1.08 2.412 -2.412 V 24.531 C 75.565 23.199 74.485 22.119 73.153 22.119 z M 33.543 81.32 c 0 1.332 -1.08 2.412 -2.412 2.412 h -2.245 c -1.332 0 -2.412 -1.08 -2.412 -2.412 V 30.799 c 0 -1.332 1.08 -2.412 2.412 -2.412 h 2.245 c 1.332 0 2.412 1.08 2.412 2.412 V 81.32 z M 48.535 81.32 c 0 1.332 -1.08 2.412 -2.412 2.412 h -2.245 c -1.332 0 -2.412 -1.08 -2.412 -2.412 V 30.799 c 0 -1.332 1.08 -2.412 2.412 -2.412 h 2.245 c 1.332 0 2.412 1.08 2.412 2.412 V 81.32 z M 63.526 81.32 c 0 1.332 -1.08 2.412 -2.412 2.412 h -2.245 c -1.332 0 -2.412 -1.08 -2.412 -2.412 V 30.799 c 0 -1.332 1.08 -2.412 2.412 -2.412 h 2.245 c 1.332 0 2.412 1.08 2.412 2.412 V 81.32 z" style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(0,0,0); fill-rule: nonzero; opacity: 1;" transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round"/></g></svg>';
+
+export const iconeMarginBottom = `
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
+<path class="ql-fill" d="M 13.55 8.55 L 13.55 7.4 1.8 7.4 1.8 8.55 13.55 8.55 M 13.05 2.55 L 13.05 1.4 1.8 1.4 1.8 2.55 13.05 2.55 M 10.55 5.55 L 10.55 4.4 1.8 4.4 1.8 5.55 10.55 5.55 M 10.3 14.65 L 10.3 10.4 1.8 10.4 1.8 14.65 10.3 14.65 M 2.8 11.4 L 9.3 11.4 9.3 13.55 2.8 13.55 2.8 11.4 M 14.6 10.55 L 13.85 9.8 11.3 12.55 13.85 15.2 14.6 14.45 12.8 12.55 14.6 10.55 Z"/>
+</svg>
+`;
+
+export const iconeTextIndent = `
+<svg width="16" height="16" xmlns="http://www.w3.org/2000/svg">
+<path class="ql-fill" d="M 1.65,2.3 0.9,3.05 2.65,4.95 0.9,6.9 1.65,7.65 4.15,4.95 1.65,2.3 M 2.5,12.45 v 1.1 h 11.25 v -1.1 H 2.5 m 11.25,-2.1 V 9.25 H 2.5 v 1.1 h 11.25 m 0,-4.3 H 6.25 V 7.1 h 7.5 V 6.05 m 0,-3.25 h -7.5 v 1.1 h 7.5 z"></path>
+</svg>
+`;
+
+export const iconeNotaDeRodape = `
+<svg width="16" height="16" fill="currentColor" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+  <path class="ql-fill" d="M 5,12.5 C 5,12.223858 5.2238576,12 5.5,12 h 2 c 0.6666664,0 0.6666664,1 0,1 h -2 C 5.2238576,13 5,12.776142 5,12.5 m 0,-2 C 5,10.223858 5.2238576,10 5.5,10 h 5 c 0.666666,0 0.666666,1 0,1 h -5 C 5.2238576,11 5,10.776142 5,10.5" />
+  <path class="ql-fill" d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2" />
+  <path class="ql-fill" d="M1 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1z" />
+</svg>
+`;
+
+export const iconeIr = `
+<svg width="14" height="14" viewBox="0 0 14 14" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
+  <circle cx="7" cy="7" r="5.5"/>
+  <polyline points="6,4.5 9,7 6,9.5"/>
+</svg>
+`;
+
+export const iconeEditar = `
+<svg width="14" height="14" viewBox="0 0 14 14" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
+  <path d="M9.5 2.5l2 2L5 11H3V9l6.5-6.5z"/>
+</svg>
+`;
+
+export const iconeRemissaoInterna = `
+<svg viewBox="0 0 18 18"><line class="ql-stroke" x1="7" x2="11" y1="7" y2="11"></line><path class="ql-even ql-stroke" d="M8.9,4.577a3.476,3.476,0,0,1,.36,4.679A3.476,3.476,0,0,1,4.577,8.9C3.185,7.5,2.035,6.4,4.217,4.217S7.5,3.185,8.9,4.577Z"></path><path class="ql-even ql-stroke" d="M13.423,9.1a3.476,3.476,0,0,0-4.679-.36,3.476,3.476,0,0,0,.36,4.679c1.392,1.392,2.5,2.542,4.679.36S14.815,10.5,13.423,9.1Z"></path></svg>
+`;
