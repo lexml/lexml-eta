7f60ddcb97361eec8e6af2ef45de41c3913981f1
João Matos <joaofmatos.10@gmail.com>
2026-07-07 18:14:41 -0300

    fix: limpeza de atributos residuais ao desformatar remissão

--- src/util/eta-quill/eta-blot-remissao-interna.ts
+++ src/util/eta-quill/eta-blot-remissao-interna.ts
@@ -42,6 +42,11 @@ export class RemissaoInternaBlot extends Inline {
 
   format(name: string, value: RemissaoInternaValue | string | boolean): void {
     if (name !== this.statics.blotName || !value) {
+      if (name === this.statics.blotName && !value) {
+        // Remove os atributos residuais ANTES do unformat padrão do Quill (InlineBlot.format):
+        this.domNode.removeAttribute('data-lexml-ref');
+        this.domNode.removeAttribute('data-ref-id');
+      }
       return super.format(name, value);
     }
 
